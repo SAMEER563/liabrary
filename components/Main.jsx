e124a8dc084845dc907ebf9770abea2891b23092
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-04-08 22:46:53 +0530

    main

---
+++ components/Main.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Image from "next/image";
+
+
+const Main = () => {
+return (
+    <>
+        <div className="">
+            <h1 className="p-10 mt-12 text-2xl text-pink-500 flex items-center justify-center">Welcome to Success Liabrary</h1>
+            <p className="">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci,
+                quod? Nisi, eos quibusdam, aliquid laudantium voluptate id ab ullam
+                eaque iste qui in tempore dolorem omnis nobis sequi perferendis veniam
+                esse eligendi minima! Nemo nihil nam, reiciendis veritatis molestiae
+                voluptatum numquam quia mollitia, eaque obcaecati vel corrupti,
+                exercitationem unde ipsam qui. Quidem officia officiis repellat
+                ratione, incidunt laborum corporis quia deserunt natus molestiae,
+                quaerat neque asperiores quisquam laudantium at ducimus voluptates.
+                Fuga, distinctio amet voluptatum id, eum animi facere quo ab aperiam
+                impedit porro deleniti ratione, vel recusandae saepe itaque? Officia
+                iusto nisi repellat facere quod sunt ad nam? Eligendi.
+            </p>
+
+           <div className=" mt-5 flex  justify-center  ">
+           <div className="ml-10">
+           <Image
+                src="/top.png"
+                width={600}
+                height={100}
+                alt="Picture of the author"
+            />
+           </div>
+           <div className="ml-5 ">
+           <Image
+                src="/mid.png"
+                width={600}
+                height={100}
+                alt="Picture of the author"
+            />
+           </div>
+           </div>
+        </div>
+    </>
+);
+};
+
+export default Main;
