86276b640e05202f9840864e314cd1b3d8536451
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-04-30 22:20:58 +0530

    change

--- components/Main.jsx
+++ components/Main.jsx
@@ -7,8 +7,8 @@ const Main = () => {
 return (
     <>
         <div className="">
-            <h1 className="p-10 mt-12 text-2xl text-pink-500 flex items-center justify-center">Welcome to Success Liabrary</h1>
-            <p className="">
+            <h1 className="p-10 mt-12 text-2xl text-pink-500 flex items-center justify-center ">Welcome to Success Liabrary</h1>
+            <p className="mb-5 text-blue-300 p-3 m-3 ">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci,
                 quod? Nisi, eos quibusdam, aliquid laudantium voluptate id ab ullam
                 eaque iste qui in tempore dolorem omnis nobis sequi perferendis veniam
@@ -22,7 +22,7 @@ return (
                 iusto nisi repellat facere quod sunt ad nam? Eligendi.
             </p>
           <div>
-          <span className="text-red-500 bg-yellow-300 flex items-center justify-center">Here is beautiful environment</span>
+          <span className="text-red-500 flex items-center justify-center ">Here is beautiful environment</span>
           </div>
 
            <div className=" mt-5 flex  justify-center  ">
