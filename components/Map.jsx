a58712b712bf38d84686e4dc89a0ebce593bed9e
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-04-30 22:16:42 +0530

    comment map page

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,54 +1,50 @@
-"use client";
-import { Loader } from '@googlemaps/js-api-loader';
-import React, { useEffect } from 'react'
+// "use client";
+// import { Loader } from '@googlemaps/js-api-loader';
+// import React, { useEffect } from 'react'
 
-function Map() {
+// function Map() {
 
-    const mapRef = React.useRef<HTMLDivElement>(null);
+//     const mapRef = React.useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-         const initMap = async () => {
-        const loader = new Loader({
-            apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY ,
-            version: "weekly"
-        });
+//     useEffect(() => {
+//          const initMap = async () => {
+//         const loader = new Loader({
+//             apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY ,
+//             version: "weekly"
+//         });
 
-        const {Map} = await loader.importLibrary("Maps");
+//         const {Map} = await loader.importLibrary("Maps");
 
-    // init marker
-    const {Marker} = await loader.importLibrary("Marker");
-        const position = {
-             lat: 37.7749, 
-             lng: -122.4194 };
-        }
+//     // init marker
+//     const {Marker} = await loader.importLibrary("Marker");
+//         const position = {
+//              lat: 37.7749, 
+//              lng: -122.4194 };
+//         }
         
-        //map options
-        const mapOptions = google.maps.MapOptions = {
-            center: position,
-            zoom: 17,
-            mapId: "MY_NEXTJS_MAPID"
-        }
+//         //map options
+//         // const mapOptions = google.maps.MapOptions = {
+//         //     center: position,
+//         //     zoom: 17,
+//         //     mapId: "MY_NEXTJS_MAPID"
+//         // }
 
-        // setup map
-        const map = new Map(mapRef.current, mapOptions);
+//         // setup map
+//         // const map = new Map(mapRef.current, mapOptions);
     
 
-        // setup marker
-        const marker = new Marker({
-            position: position,
-            map: map,
-            title: "My NextJS Map"
-    });
-    initMap();
-}, []);
+//         // setup marker
+//         const marker = new Marker({
+//             position: position,
+//             map: map,
+//             title: "My NextJS Map"
+//     });
+//     initMap();
+// }, []);
 
-    } [];
-
-    return (
-    <div style={{height: '600px'}} ref={mapRef} />
+//     } [];
 
     
-    )
 
 
-export default Map
+// export default Map
